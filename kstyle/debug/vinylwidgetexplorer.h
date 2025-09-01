1df6e50000a5a4bfb8965cd5d2db81b9cfe708f4
Christian Tosta <7252968+christiantosta@users.noreply.github.com>
2025-09-01 03:03:46 -0300

    Sync debug with Breeze 6.4.80

--- kstyle/debug/vinylwidgetexplorer.h
+++ kstyle/debug/vinylwidgetexplorer.h
@@ -1,79 +1,59 @@
-#ifndef vinylwidgetexplorer_h
-#define vinylwidgetexplorer_h
+/*
+ * SPDX-FileCopyrightText: 2014 Hugo Pereira Da Costa <hugo.pereira@free.fr>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 
-/*************************************************************************
- * Copyright (C) 2014 by Hugo Pereira Da Costa <hugo.pereira@free.fr>    *
- *                                                                       *
- * This program is free software; you can redistribute it and/or modify  *
- * it under the terms of the GNU General Public License as published by  *
- * the Free Software Foundation; either version 2 of the License, or     *
- * (at your option) any later version.                                   *
- *                                                                       *
- * This program is distributed in the hope that it will be useful,       *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- * GNU General Public License for more details.                          *
- *                                                                       *
- * You should have received a copy of the GNU General Public License     *
- * along with this program; if not, write to the                         *
- * Free Software Foundation, Inc.,                                       *
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
- *************************************************************************/
+#pragma once
 
 #include <QEvent>
-#include <QObject>
 #include <QMap>
+#include <QObject>
 #include <QSet>
 #include <QWidget>
 
 namespace Vinyl
 {
+//* print widget's and parent's information on mouse click
+class WidgetExplorer : public QObject
+{
+    Q_OBJECT
 
-    //* print widget's and parent's information on mouse click
-    class WidgetExplorer: public QObject
+public:
+    //* constructor
+    explicit WidgetExplorer(QObject*);
+
+    //* enable
+    bool enabled() const;
+
+    //* enable
+    void setEnabled(bool);
+
+    //* widget rects
+    void setDrawWidgetRects(bool value)
     {
+        _drawWidgetRects = value;
+    }
 
-        Q_OBJECT
+    //* event filter
+    bool eventFilter(QObject *, QEvent *) override;
 
-        public:
+protected:
+    //* event type
+    QString eventType(const QEvent::Type &) const;
 
-        //* constructor
-        explicit WidgetExplorer( QObject* );
+    //* print widget information
+    QString widgetInformation(const QWidget *) const;
 
-        //* enable
-        bool enabled() const;
+private:
+    //* enable state
+    bool _enabled = false;
 
-        //* enable
-        void setEnabled( bool );
+    //* widget rects
+    bool _drawWidgetRects = false;
 
-        //* widget rects
-        void setDrawWidgetRects( bool value )
-        { _drawWidgetRects = value; }
-
-        //* event filter
-        bool eventFilter( QObject*, QEvent* ) override;
-
-        protected:
-
-        //* event type
-        QString eventType( const QEvent::Type& ) const;
-
-        //* print widget information
-        QString widgetInformation( const QWidget* ) const;
-
-        private:
-
-        //* enable state
-        bool _enabled = false;
-
-        //* widget rects
-        bool _drawWidgetRects = false;
-
-        //* map event types to string
-        QMap<QEvent::Type, QString > _eventTypes;
-
-    };
+    //* map event types to string
+    QMap<QEvent::Type, QString> _eventTypes;
+};
 
 }
-
-#endif
